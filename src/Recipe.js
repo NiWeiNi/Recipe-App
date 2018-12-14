c945003fd3ea70d9fdb9354a2241c5714a014e01
Wei <weinini007@gmail.com>
2018-12-14 21:14:39 +1100

    feat: create recipe component

---
+++ src/Recipe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+    render() {
+        const {title} = this.props;
+        return(
+            <div>Recipe {title}</div>
+        );
+    }
+}
+
+export default Recipe;
